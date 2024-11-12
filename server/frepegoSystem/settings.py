ee1ec3d2378bfa01ebd9403cb14fbc5a74a9fd4f
Faith-qa <atienofaith12@gmail.com>
2024-11-12 07:59:16 +0300

    deployment fix

--- server/frepegoSystem/settings.py
+++ server/frepegoSystem/settings.py
@@ -16,6 +16,11 @@ import django
 import environ
 from django.utils.translation import gettext
 django.utils.translation.ugettext = gettext
+import mimetypes
+
+mimetypes.add_type("text/javascript", ".js", True)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
