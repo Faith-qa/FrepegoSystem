669b990bb41f83c3bd33c7c1eb8d493127ea2359
Faith-qa <atienofaith12@gmail.com>
2024-11-12 06:01:12 +0300

    added deployment url to allowed_host

--- server/frepegoSystem/settings.py
+++ server/frepegoSystem/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-nb78yc7$i+ui+82)3wcmn-$852knj#zfyb1nyh6=bydg69k^ke
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1']
+ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'frepegosystem.onrender.com']
 
 
 # Application definition
